c3da690938fe0c008aa68440f0251e140d2b4626
3bdulrahman-M3 <abdu1234680@gmail.com>
2025-08-21 20:35:24 +0300

    Refactor NotificationConsumer and NotificationSerializer for improved authentication handling and readability

--- BackEnd/App/notifications/serializers.py
+++ BackEnd/App/notifications/serializers.py
@@ -3,36 +3,28 @@ from .models import Notification
 
 
 class NotificationSerializer(serializers.ModelSerializer):
-<<<<<<< HEAD
-    class Meta:
-        model = Notification
-        fields = ['id', 'notification_type', 'title',
-                  'message', 'data', 'is_read', 'created_at']
-        read_only_fields = ['id', 'created_at']
-=======
     sender_name = serializers.SerializerMethodField()
     receiver_name = serializers.SerializerMethodField()
-    
+
     class Meta:
         model = Notification
         fields = [
-            'id', 
-            'sender', 
+            'id',
+            'sender',
             'sender_name',
-            'receiver', 
+            'receiver',
             'receiver_name',
-            'notification_type', 
+            'notification_type',
             'title',
-            'message', 
-            'data', 
-            'is_read', 
+            'message',
+            'data',
+            'is_read',
             'created_at'
         ]
         read_only_fields = ['id', 'created_at', 'sender', 'receiver']
-    
+
     def get_sender_name(self, obj):
         return obj.sender.get_full_name() if obj.sender else "System"
-    
+
     def get_receiver_name(self, obj):
-        return obj.receiver.get_full_name()
->>>>>>> origin/notification
+        return obj.receiver.get_full_name() if obj.receiver else ""
